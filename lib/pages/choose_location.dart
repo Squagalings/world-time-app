bbec87b70c90b6a70ca5cfa8d212b6ae298bef32
squagalings <justineyongjzxy@gmail.com>
2021-10-14 18:00:30 -0700

    lesson-34

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:world_time_app/services/world_time.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,16 +7,35 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<WorldTime> locations = [
+    WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+    WorldTime(url: 'Germany/Berlin', location: 'Athens', flag: 'greece.png'),
+    WorldTime(url: 'Egypt/Cairo', location: 'Cairo', flag: 'kenya.png'),
+    WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    WorldTime(url: 'America/New_York', location: '', flag: 'usa.png'),
+    WorldTime(url: 'Asia/Seoul', location: '', flag: 'south_korea.png'),
+    WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('Choose a Location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-    ); // Scaffold
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Choose a Location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: ListView.builder(
+            itemCount: locations.length,
+            itemBuilder: (context, index) {
+              return Card(
+                child: ListTile(
+                  onTap: () {},
+                  title: Text(locations[index].location),
+                ),
+              );
+            })); // Scaffold
   }
 }
